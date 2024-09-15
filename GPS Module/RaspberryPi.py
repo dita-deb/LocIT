da9308a109cb949a5e976c79a9a8b9de2ffcfb85
ponder-zachary <167157814+ponder-zachary@users.noreply.github.com>
2024-09-14 21:35:42 -0400

    Update RaspberryPi.py

--- GPS Module/RaspberryPi.py
+++ GPS Module/RaspberryPi.py
@@ -3,31 +3,28 @@ import board
 import busio
 import adafruit_gps
 
-# uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
-# not needed as that's for i2c, we will be utilizing uart
+# For I2C, use the default pins for SCL and SDA on the Raspberry Pi.
+i2c = busio.I2C(board.SCL, board.SDA)
 
-import serial
-uart = serial.Serial("/dev/serial0", baudrate=9600, timeout=10)
+# Create a GPS module instance using the I2C interface.
+gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)  # Use I2C interface
 
-# Create a GPS module instance.
-gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
-# gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)  # Use I2C interface
-
+# Initialize the GPS module by changing what data it sends and at what rate.
 # Turn on the basic GGA and RMC info (what you typically want)
 gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
-# Turn on just minimum info (RMC only, location):
-# gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Turn off everything:
-# gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Turn on everything (not all of it is parsed!)
-# gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
-
-# Set update rate to once a second (1hz) which is what you typically want.
+# Set update rate to once a second (1hz).
 gps.send_command(b"PMTK220,1000")
 
 # Main loop runs forever printing the location, etc. every second.
 last_print = time.monotonic()
+
+# Conversion factors
+METERS_TO_FEET = 3.28084
+KNOTS_TO_MPH = 1.15078
+
 while True:
+    # Make sure to call gps.update() every loop iteration and at least twice
+    # as fast as data comes from the GPS unit (usually every second).
     gps.update()
     # Every second print out current location details if there's a fix.
     current = time.monotonic()
@@ -37,43 +34,32 @@ while True:
             # Try again if we don't have a fix yet.
             print("Waiting for fix...")
             continue
-
+        
+        # We have a fix! Now print out the relevant information.
         print("=" * 40)  # Print a separator line.
+        
+        # Timestamp
         print(
-            "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
-                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
-                gps.timestamp_utc.tm_mday,  # struct_time object that holds
-                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-                gps.timestamp_utc.tm_min,  # month!
+            "Timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+                gps.timestamp_utc.tm_mon,  
+                gps.timestamp_utc.tm_mday,  
+                gps.timestamp_utc.tm_year,  
+                gps.timestamp_utc.tm_hour,  
+                gps.timestamp_utc.tm_min,  
                 gps.timestamp_utc.tm_sec,
             )
         )
+        
+        # Latitude and Longitude
         print("Latitude: {0:.6f} degrees".format(gps.latitude))
         print("Longitude: {0:.6f} degrees".format(gps.longitude))
-        print(
-            "Precise Latitude: {} degs, {:2.4f} mins".format(
-                gps.latitude_degrees, gps.latitude_minutes
-            )
-        )
-        print(
-            "Precise Longitude: {} degs, {:2.4f} mins".format(
-                gps.longitude_degrees, gps.longitude_minutes
-            )
-        )
-        print("Fix quality: {}".format(gps.fix_quality))
         
-        if gps.satellites is not None:
-            print("# satellites: {}".format(gps.satellites))
+        # Altitude (in feet)
         if gps.altitude_m is not None:
-            print("Altitude: {} meters".format(gps.altitude_m))
+            altitude_ft = gps.altitude_m * METERS_TO_FEET
+            print("Altitude: {:.2f} feet".format(altitude_ft))
+        
+        # Speed (in miles per hour)
         if gps.speed_knots is not None:
-            print("Speed: {} knots".format(gps.speed_knots))
-        if gps.track_angle_deg is not None:
-            print("Track angle: {} degrees".format(gps.track_angle_deg))
-        if gps.horizontal_dilution is not None:
-            print("Horizontal dilution: {}".format(gps.horizontal_dilution))
-        if gps.height_geoid is not None:
-            print("Height geoid: {} meters".format(gps.height_geoid))
-            
-       # To add NMEA data parsing soon.
+            speed_mph = gps.speed_knots * KNOTS_TO_MPH
+            print("Speed: {:.2f} mph".format(speed_mph))
